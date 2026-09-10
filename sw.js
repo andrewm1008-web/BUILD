34ce9a3c32b117091e72e52eed8fb366f7cb61e6
andrewm1008-web <andrewm1008@gmail.com>
2026-09-10 18:26:40 +0100

    Cache all BUILD app modules for offline use

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='build-v0.4.0';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest'];
+const CACHE='build-v0.4.1';
+const ASSETS=['./','./index.html','./styles.css','./app.js','./profile-client.js','./plan-import-client.js','./strava-client.js','./manifest.webmanifest'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(hit=>hit||caches.match('./index.html'))))});
